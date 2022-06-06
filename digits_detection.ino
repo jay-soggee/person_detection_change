e4d135f2f1bc3ff186c440c2b76abd7c7db647ee
Bear_Jay <jaesuki98@gmail.com>
2022-06-06 13:56:44 +0900

    add run_model()

---
+++ digits_detection.ino
@@ -0,0 +1,114 @@
+/* Copyright 2019 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#include <TensorFlowLite.h>
+
+#include "main_functions.h"
+
+#include "detection_responder.h"
+#include "image_provider.h"
+#include "model_settings.h"
+#include "model.h"
+#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/micro_interpreter.h"
+#include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/schema/schema_generated.h"
+#include "tensorflow/lite/version.h"
+
+
+namespace {
+tflite::ErrorReporter* error_reporter = nullptr;
+const tflite::Model* model = nullptr;
+tflite::MicroInterpreter* interpreter = nullptr;
+TfLiteTensor* input = nullptr;
+
+constexpr size_t kTensorArenaSize = 136 * 1024;
+static uint8_t tensor_arena[kTensorArenaSize];
+}  // namespace
+
+
+void setup() {
+
+    delay(10000);
+
+    static tflite::MicroErrorReporter micro_error_reporter;
+    error_reporter = &micro_error_reporter;
+
+
+    model = tflite::GetModel(g_model);
+    if (model->version() != TFLITE_SCHEMA_VERSION) {
+      TF_LITE_REPORT_ERROR(error_reporter,
+                          "Model provided is schema version %d not equal "
+                          "to supported version %d.",
+                          model->version(), TFLITE_SCHEMA_VERSION);
+      return;
+    }
+
+    static tflite::MicroMutableOpResolver<6> micro_op_resolver;
+    micro_op_resolver.AddConv2D();
+    micro_op_resolver.AddDepthwiseConv2D();
+    micro_op_resolver.AddFullyConnected();
+    micro_op_resolver.AddMaxPool2D();
+    micro_op_resolver.AddReshape();
+    micro_op_resolver.AddSoftmax();
+
+    static tflite::MicroInterpreter static_interpreter(
+        model, 
+        micro_op_resolver, 
+        tensor_arena, 
+        kTensorArenaSize, 
+        error_reporter);
+    interpreter = &static_interpreter;
+
+
+    TfLiteStatus allocate_status = interpreter->AllocateTensors();
+    if (allocate_status != kTfLiteOk) {
+      TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+      return;
+    }
+
+
+    input = interpreter->input(0);
+}
+
+
+int run_model() {
+    int result;
+
+    if (kTfLiteOk != GetImage(error_reporter, kNumCols, kNumRows, kNumChannels, input->data.int8)) {
+        TF_LITE_REPORT_ERROR(error_reporter, "Image capture failed.");
+    }
+
+    if (kTfLiteOk != interpreter->Invoke()) {
+        TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed.");
+    }
+    
+    TfLiteTensor* output = interpreter->output(0);
+
+    for (int i; i < 10; i++) 
+        if (output->data.int8[result] < output->data.int8[i])
+            result = i;
+
+    return result;
+}
+
+
+void loop() {
+
+  int predict = run_model();
+  TF_LITE_REPORT_ERROR(error_reporter, "predict as : %d", predict);
+
+}
